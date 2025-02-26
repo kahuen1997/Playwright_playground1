dc99de47cda11fac4bb03906639dc613facab286
MonMon1997 <monmonmonmonmon11112@gmail.com>
2025-02-26 10:17:02 +0800

    14 commits - add new playwright.yml for shard test

--- playwright.config.js
+++ playwright.config.js
@@ -20,11 +20,11 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 1,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: process.env.CI ? "blob" : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -38,7 +38,7 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: { ...devices["Desktop Chrome"], channel: "chrome" },
     },
 
     {
@@ -48,9 +48,15 @@ export default defineConfig({
 
     {
       name: "webkit",
-      use: { ...devices["Desktop Safari"] },
+      use: { ...devices["Desktop Safari"], isMobile: false },
     },
 
+    // {
+    //   name: "Mobile Safari",
+    //   use: {
+    //     ...devices["iPhone 13"],
+    //   },
+    // },
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
